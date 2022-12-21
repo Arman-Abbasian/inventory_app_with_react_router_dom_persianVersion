5f11dd390f2979661657da35260988316a59f088
Arman Abbasian <105098083+Arman-Abbasian@users.noreply.github.com>
2022-12-21 09:34:38 +0330

    make ExitList & OneExitItem component

--- src/pages/ExitList.jsx
+++ src/pages/ExitList.jsx
@@ -1,7 +1,40 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { toast } from "react-hot-toast";
+import OneExitItem from "../components/OneExitItem";
+
 const ExitList = () => {
+    const [exitList,setExitList]=useState({data:null,error:null,loading:false});
+    async  function getEnterList(){
+        setExitList({data:null,error:null,loading:true})
+        try {
+        const {data}= await axios.get(`http://localhost:4000/exit`) 
+        setExitList({data:data,error:null,loading:false})
+        } catch (err) {
+            setExitList({data:null,error:err.message,loading:false})
+          toast.error(err.message)  
+        }
+    }
+    useEffect(()=>{getEnterList()},[]);
+    const deleteHandler=(id)=>{
+        axios.delete(`http://localhost:4000/exit/${id}`)
+        .then(res=>{
+            toast.success('data removed successfully');
+            getEnterList();
+        })
+        .catch(err=>toast.error(err.message))
+    }
+    console.log('exitList :', exitList.data);
     return ( 
-        <div>exit list</div>
+        <div className="flex flex-col gap-y-4">
+            {exitList.data && 
+            exitList.data.map(item=>(
+                <OneExitItem key={item.id} id={item.id}productName={item.productName}productSpecification={item.productSpecification}
+                number={item.number}measurmentUnit={item.measurmentUnit}date={item.date} consumingFor={item.consumingFor}
+                exitDelivery={item.exitDelivery} exitTransferee={item.exitTransferee} jobPosition={item.jobPosition} unit={item.unit} deleteHandle={deleteHandler} />
+            ))
+            }
+        </div>
      );
-}
- 
+} 
 export default ExitList;
