55d779516a49996ded2fde75ac3e5512801142d8
Arman Abbasian <105098083+Arman-Abbasian@users.noreply.github.com>
2023-01-18 12:23:51 +0330

    some changs

---
+++ src/components/FilterPurchase/FilterPurchaseRequestList.jsx
@@ -0,0 +1,100 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { toast } from "react-hot-toast";
+
+const FilterPurchaseRequestList = ({
+  changeHandler,
+  toggleChangeDateHandler,
+  selectedProduct,
+  latest,
+}) => {
+  //make options for 'select options' and 'search select' filters
+  //1- overall products
+  const [productNames, setProductNames] = useState({
+    data: null,
+    error: null,
+    loading: false,
+  });
+
+  // fill the productName state
+  useEffect(() => {
+    setProductNames({ data: null, error: null, loading: true });
+    axios
+      .get(`http://localhost:4000/overall?category=productName`)
+      .then((res) =>
+        setProductNames({ data: res.data, error: null, loading: false })
+      )
+      .catch((err) => {
+        toast.error(err.message);
+        setProductNames({
+          data: null,
+          error: err.message,
+          loading: false,
+        });
+      });
+  }, []);
+  return (
+    <>
+      {productNames.data && (
+        <div className="grid sm:grid-cols-2 container mx-auto max-w-2xl gap-4 w-full">
+          {/* make search select for productNames */}
+          <div className="flex flex-col items-start justify-center">
+            <label
+              className="mb-1 text-primary_light_green"
+              htmlFor="productName"
+            >
+              select a products
+            </label>
+            <input
+              placeholder="Search product name..."
+              id="productNamee"
+              className="w-full bg-transparent p-2 border text-primary_cream rounded focus:outline-none"
+              list="productName"
+              name="productName"
+              onChange={(e) => changeHandler(e)}
+              value={selectedProduct}
+            />
+            <datalist id="productName">
+              <option value="" className="bg-primary_yellow">
+                All
+              </option>
+              {productNames.data.map((item) => (
+                <option
+                  key={item.id}
+                  value={item.productName}
+                  className="bg-primary_cream"
+                >
+                  {item.productName}
+                </option>
+              ))}
+            </datalist>
+          </div>
+
+          {/* ascending or descending based on date */}
+          <div className="w-full">
+            <label className="inline-flex relative items-center cursor-pointer">
+              <input
+                type="checkbox"
+                value=""
+                className="sr-only peer"
+                name="latest"
+                checked={latest}
+                onChange={(e) => toggleChangeDateHandler(e)}
+              />
+              <div
+                className="w-11 h-6 bg-primary_light_green peer-focus:outline-none
+                        rounded-md peer  peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px]
+                        after:bg-white after:border-gray-300 after:border after:rounded-md after:h-5 after:w-5  after:transition-all  peer-checked:bg-primary_yellow"
+              ></div>
+              <span className="ml-3 text-sm font-medium text-primary_cream">
+                {latest ? "latest" : "earliest"}
+              </span>
+            </label>
+          </div>
+        </div>
+      )}
+    </>
+  );
+};
+
+export default FilterPurchaseRequestList;
--- src/pages/PurchasingRequestList.jsx
+++ src/pages/PurchasingRequestList.jsx
@@ -2,21 +2,99 @@ import axios from "axios";
 import { useEffect, useState } from "react";
 import { toast } from "react-hot-toast";
 import { Link } from "react-router-dom";
+import FilterPurchaseRequestList from "../components/FilterPurchase/FilterPurchaseRequestList";
 import OnePurchaseItem from "../components/PurchaseComponent/OnePurchaseItem ";
 
 const PurchasingRequestList = () => {
-  const [allPurchasingRequests, setAllPurchasingRequest] = useState(null);
+  const [allPurchasingRequests, setAllPurchasingRequest] = useState({
+    data: null,
+    error: null,
+    loading: false,
+  });
+  const [showAllPurchasingRequests, setShowAllPurchasingRequests] =
+    useState(null);
+  const [sum, setSum] = useState(0);
+
+  const [filters, setFilters] = useState({
+    productName: "",
+    latest: true,
+  });
   useEffect(() => {
+    setAllPurchasingRequest({ data: null, error: null, loading: true });
     axios
       .get(`http://localhost:4000/purchasingReequests`)
-      .then((res) => setAllPurchasingRequest(res.data))
-      .catch((err) => toast.error(err.message));
+      .then((res) =>
+        setAllPurchasingRequest({ data: res.data, error: null, loading: false })
+      )
+      .catch((err) => {
+        setAllPurchasingRequest({
+          data: null,
+          error: err.message,
+          loading: false,
+        });
+        toast.error(err.message);
+      });
   }, []);
+  useEffect(() => {
+    if (allPurchasingRequests.data) {
+      let show = [...allPurchasingRequests.data];
+      show = filterWithAllPurchasingRequests(show);
+      show = sortDate(show);
+      setShowAllPurchasingRequests(show);
+      //calculate sum
+      const summ = show.reduce(
+        (accumulator, currentValue) => accumulator + currentValue.number,
+        0
+      );
+      setSum(summ);
+      console.log(summ);
+    }
+  }, [filters, allPurchasingRequests.data]);
 
+  const changeHandler = (e) => {
+    setFilters({ ...filters, [e.target.name]: e.target.value });
+  };
+  const toggleChangeDateHandler = (e) => {
+    console.log(e.target.checked);
+    setFilters({ ...filters, latest: e.target.checked });
+  };
+  function filterWithAllPurchasingRequests(arr) {
+    if (filters.productName === "") {
+      return arr;
+    } else {
+      arr = arr.filter((item) => item.productName === filters.productName);
+      return arr;
+    }
+  }
+  function sortDate(arr) {
+    if (filters.latest) {
+      arr.sort(function (a, b) {
+        const date1 = new Date(a.date);
+        const date2 = new Date(b.date);
+        return date2 - date1;
+      });
+    } else {
+      arr.sort(function (a, b) {
+        const date1 = new Date(a.date);
+        const date2 = new Date(b.date);
+        return date1 - date2;
+      });
+    }
+    return arr;
+  }
   return (
-    <div>
-      {allPurchasingRequests &&
-        allPurchasingRequests.map((item) => (
+    <div className="lg:flex-1">
+      <FilterPurchaseRequestList
+        changeHandler={changeHandler}
+        toggleChangeDateHandler={toggleChangeDateHandler}
+        selectedProduct={filters.productName}
+        latest={filters.latest}
+      />
+      <p className="bg-primary_light_green inline-block p-2 rounded mb-10 mt-10 shadow-sm shadow-primary_cream">
+        sum: {sum}
+      </p>
+      {showAllPurchasingRequests &&
+        showAllPurchasingRequests.map((item) => (
           <Link to={`/PurchasingRequestList/${item.id}`} key={item.id}>
             <OnePurchaseItem
               productName={item.productName}
